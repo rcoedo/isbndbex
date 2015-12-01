8f9b34e58118539499438f0127b98e01cb26e6d4
Roman Coedo <romancoedo@gmail.com>
2015-12-02 00:32:34 +0100

    GenServer first approach

--- lib/isbndbex.ex
+++ lib/isbndbex.ex
@@ -1,2 +1,17 @@
 defmodule Isbndbex do
+  use GenServer
+
+  alias Isbndbex.Api, as: Api
+
+  def start(key) do
+    GenServer.start_link(__MODULE__, key)
+  end
+
+  def get_book(pid, id) do
+    GenServer.call(pid, {:get_book, id})
+  end
+
+  def handle_call({:get_book, id}, _, key) do
+    {:reply, Api.get_book(key, id), key}
+  end
 end
